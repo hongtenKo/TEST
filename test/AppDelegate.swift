28e77994f6eed72bedd7b53a48c5eb2261f0c0a1
柯昊瑋 <hongten.ko@alfred.camera>
2019-04-09 16:50:03 +0800

    asdasdasdasdad

--- test/AppDelegate.swift
+++ test/AppDelegate.swift
@@ -15,7 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        //ㄋ
+        //ㄋㄋasdasdasdasd
         // Override point for customization after application launch.
         return true
     }
